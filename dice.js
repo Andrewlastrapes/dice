7c9a506df030ebc6e8bb5755e007d32dc826c863
Andrew Lastrapes <Andrewlastrapes@gmail.com>
2017-07-20 14:13:37 -0400

    added dice images

--- dice.js
+++ dice.js
@@ -1,7 +1,5 @@
-var button = document.querySelector(".button");
-var dice = document.querySelector(".dice"); 
-
-
+var $button = $(".button");
+var $dice = $(".dice"); 
 
 
 
@@ -9,21 +7,97 @@ var dice = document.querySelector(".dice");
 function randomNum(){
 	var number = [];
 	number.push(Math.ceil(Math.random() * 6))
-	return number
+	return one()
 }
+
+
+function one(){
+
+	var imgOne = $('<img />', {
+		"class": "dice",
+		"src": "one.png"
+	});
+
+	imgOne.appendTo($('.dice'))
+}
+
+function two(){
+
+	var imgOne = $('<img />', {
+		"class": "dice",
+		"src": "two.png"
+	});
+
+	imgOne.appendTo($('.dice'))
+}
+
+function three(){
+
+	var imgOne = $('<img />', {
+		"class": "dice",
+		"src": "three.png"
+	});
+
+	imgOne.appendTo($('.dice'))
+}
+
+function four(){
+
+	var imgOne = $('<img />', {
+		"class": "dice",
+		"src": "four.png"
+	});
+
+	imgOne.appendTo($('.dice'))
+}
+
+function five(){
+
+	var imgOne = $('<img />', {
+		"class": "dice",
+		"src": "five.png"
+	});
+
+	imgOne.appendTo($('.dice'))
+}
+
+function six(){
+
+	var imgOne = $('<img />', {
+		"class": "dice",
+		"src": "six.png"
+	});
+
+	imgOne.appendTo($('.dice'))
+}
+
+
 
 
 function roll(fun){
 	var x = fun()
-	dice.textContent = x
+	$dice.text(x) 
 }
 
 
 function click(thebutton){
-	thebutton.addEventListener("click", function(event){
+	thebutton.on("click", function(event){
 		event.preventDefault();
 		roll(randomNum)
 	})
 }
 
-click(button)
+
+var imageDice = []
+var imageDice = {
+	1: "one.png",
+	2: "two.png",
+	3: "three.png",
+	4: "four.png",
+	5: "five.png",
+	6: "six.png"
+
+}
+
+
+click($button)
